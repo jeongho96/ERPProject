c79e24febadc6222f5ee0f40ca05b764c8f01f38
LAPTOP-36TFAG1H\user04 <soyamilk0705@naver.com>
2023-08-30 11:54:57 +0900

    create: Management의 ApiControoler, Mapper, Service. XML 생성

---
+++ src/main/java/com/chonamzone/erpproject/controller/api/ManagementApiController.java
@@ -0,0 +1,20 @@
+package com.chonamzone.erpproject.controller.api;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chonamzone.erpproject.service.ManagementService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ManagementApiController {
+	
+	private final ManagementService managementService;
+	
+
+}
---
+++ src/main/java/com/chonamzone/erpproject/service/ManagementService.java
@@ -0,0 +1,17 @@
+package com.chonamzone.erpproject.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.chonamzone.erpproject.mapper.ManagementMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ManagementService {
+	
+	private final ManagementMapper managementMapper;
+		
+}
