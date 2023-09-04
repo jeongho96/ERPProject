1dde9233212860accb37e0ccfde76a4cf88def51
LAPTOP-36TFAG1H\user04 <soyamilk0705@naver.com>
2023-09-04 09:40:58 +0900

    feat: DocumentListDTO innerClass 생성

--- src/main/java/com/chonamzone/erpproject/model/DocumentListDTO.java
+++ src/main/java/com/chonamzone/erpproject/model/DocumentListDTO.java
@@ -7,21 +7,53 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
 public class DocumentListDTO {
 	
-	private  int dSeq;								// 기안서 식별자 
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class MapperData {
+		private int dSeq;								// 결재라인 식별자
+		private String dDraftingDate;					// 기안 일자
+		private String dStatus;							// 진행 상태
+		private int dDrafterId;							// 기안자
+		private String dCategory;						// 문서 종류 (휴가신청서/출장보고서)
+		private int aApproverId;						// 결재자
+		private int aOrderNum;							// 결재 순서
+		private String aApproverState;					// 결재 여부
+	}
 	
-	private String dDraftingDate;				// 기안 일자
 	
-	private String dStatus;						// 진행 상태
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class Response {
+		private int dSeq;
+		private String dDraftingDate;					// 기안 일자
+		private String dStatus;							// 진행 상태
+		private String dDrafterName;					// 기안자 이름
+		private String dCategory;						// 문서 종류 (휴가신청서/출장보고서)
+		private String aApproverName;					// 결재자 이름
+		private int aOrderNum;							// 결재 순서
+		private String aApproverState;					// 결재 여부
+		
 	
-	private int dDrafterId;						// 기안자
+		public Response(MapperData mapperData) {
+			this.dSeq = mapperData.getDSeq();
+			this.dDraftingDate = mapperData.getDDraftingDate();
+			this.dStatus = mapperData.getDStatus();
+			this.dCategory = mapperData.getDCategory();
+			this.aOrderNum = mapperData.getAOrderNum();
+			this.aApproverState = mapperData.getAApproverState();
+		}
+		
+		public void idToName(String drafterName, String approverName) {
+			this.dDrafterName = drafterName;
+			this.aApproverName = approverName;
+		}
 	
-	private String dCatetory;					// 문서 종류 (휴가신청서/출장보고서)
+	}
 	
-	private List<ApproversDTO> approversList;
-
+	
 }
