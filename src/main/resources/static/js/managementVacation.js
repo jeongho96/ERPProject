4843c2743b0dbd7b9cce65e86b315aa05897eed1
Heesoo <soyamilk0705@naver.com>
2023-09-04 21:49:16 +0900

    feat: 통합 관리 휴가보고서 상세보기 추가

---
+++ src/main/resources/static/js/managementVacation.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(){
+	
+	var dSeq = location.pathname;
+	dSeq = dSeq.replace("/management/vacations/", "");
+	
+
+
+	$.ajax({
+		url: "/api/management/vacations/" + dSeq,
+		type: "GET"
+	}).done(function(data){
+		
+		document.getElementById("startDate").value = data.vstartDate;
+		
+		
+		/*{
+			"vendDate": "2023-02-06 00:00:00",
+			"vreason": "휴가 신청합니다.",
+			"vseq": 0,
+			"vstartDate": "2023-02-05 00:00:00",
+			"vleaveType": "연차",
+			"vemployeeContact": "010-2222-2223"
+		}*/
+	})
+
+
+});
