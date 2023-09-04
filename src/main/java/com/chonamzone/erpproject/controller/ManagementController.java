28d40db23b235655705911750fa3dcc34e539fdd
Heesoo <soyamilk0705@naver.com>
2023-09-04 21:49:55 +0900

    feat: 통합 관리 출장보고서 상세보기 추가

--- src/main/java/com/chonamzone/erpproject/controller/ManagementController.java
+++ src/main/java/com/chonamzone/erpproject/controller/ManagementController.java
@@ -2,13 +2,24 @@ package com.chonamzone.erpproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ManagementController {
 	
-	@GetMapping("/management")
-	public String managementList() {
-		return "managementList";
+	@GetMapping("/management/all")
+	public String managementAllList() {
+		return "managementAllList";
+	}
+	
+	@GetMapping("/management/travels/{dSeq}")
+	public String managementTravel(@PathVariable int dSeq) {
+		return "managementTravel";
+	}
+	
+	@GetMapping("/management/vacations/{dSeq}")
+	public String managementVacation(@PathVariable int dSeq) {
+		return "managementVacation";
 	}
 	
 	@GetMapping("/test")
