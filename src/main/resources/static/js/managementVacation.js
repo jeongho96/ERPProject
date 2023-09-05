65e8cefd4503414a1ab135b1795d77dce7ab490b
LAPTOP-36TFAG1H\user04 <soyamilk0705@naver.com>
2023-09-05 16:58:19 +0900

    update: management 상세보기 view 수정

--- src/main/resources/static/js/managementVacation.js
+++ src/main/resources/static/js/managementVacation.js
@@ -7,19 +7,55 @@ document.addEventListener("DOMContentLoaded", function(){
 		url: "/api/management/vacations/" + dSeq,
 		type: "GET"
 	}).done(function(data){
-		document.getElementById("startDate").value = data.vstartDate;
-	
+		document.getElementById("dateInput").value = data.documentListDTO.ddraftingDate;
+		document.getElementById("startDate").value = data.vacationDTO.vstartDate;
+		document.getElementById("endDate").value = data.vacationDTO.vendDate;
+		document.getElementById("partname").value = data.userDTO.pname;
+		document.getElementById("name").value = data.userDTO.uname;
+		document.getElementById("position").value = data.userDTO.uposition;
+		document.getElementById("reason").value = data.vacationDTO.vreason;
+		document.getElementById("emergencyContact").value = data.vacationDTO.vemployeeContact;
+		document.getElementById(data.vacationDTO.vleaveType).checked = true;
 		
+		/*
+		{
+			"documentListDTO": {
+				"ddrafterId": 1,
+				"aapproverId": 0,
+				"dstatus": "진행중",
+				"dcategory": "휴가신청서",
+				"dseq": 1,
+				"ddraftingDate": "2023-02-03",
+				"aorderNum": 0,
+				"aapproverState": null
+			},
+			"vacationDTO": {
+				"vseq": 0,
+				"vleaveType": "연차",
+				"vreason": "휴가 신청합니다.",
+				"vstartDate": "2023-02-05",
+				"vendDate": "2023-02-06",
+				"vemployeeContact": "010-2222-2223"
+			},
+			"approverList": [
+				{
+					"aapproverId": 4,
+					"pname": "개발팀",
+					"uname": "김팀장"
+				},
+				{
+					"aapproverId": 5,
+					"pname": "개발팀",
+					"uname": "김사장"
+				}
+			],
+			"userDTO": {
+				"pname": "개발팀",
+				"uname": "김인턴",
+				"uposition": "인턴"
+			}
+		}
+		*/
 		
-		/*{
-			"vendDate": "2023-02-06 00:00:00",
-			"vreason": "휴가 신청합니다.",
-			"vseq": 0,
-			"vstartDate": "2023-02-05 00:00:00",
-			"vleaveType": "연차",
-			"vemployeeContact": "010-2222-2223"
-		}*/
 	})
-
-
 });
