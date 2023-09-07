4ed248ff943924720a9999a2df96c6067df14ad0
LAPTOP-K3H9G676\user14 <whskagur7@gmail.com>
2023-09-07 18:16:20 +0900

    Update MyApprovalController.java

--- src/main/java/com/chonamzone/erpproject/controller/MyApprovalController.java
+++ src/main/java/com/chonamzone/erpproject/controller/MyApprovalController.java
@@ -2,13 +2,15 @@ package com.chonamzone.erpproject.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,6 +18,8 @@ import com.chonamzone.erpproject.model.MyApprovalDTO;
 import com.chonamzone.erpproject.model.MyApprovalDTO2;
 import com.chonamzone.erpproject.model.MyApprovalDTO3;
 import com.chonamzone.erpproject.model.MyApprovalStateDO;
+import com.chonamzone.erpproject.model.UserDTO;
+import com.chonamzone.erpproject.service.ManagementService;
 import com.chonamzone.erpproject.service.MyApprovalService;
 
 
@@ -24,13 +28,15 @@ public class MyApprovalController {
 	
 	@Autowired
 	private MyApprovalService myApprovalService;
-	
-	int loginid = 4;
+	private ManagementService managementService;
 	
 	
 	@GetMapping("/myApprovalList")
-    public String myApprovalList(@RequestParam(defaultValue = "1") int page, Model model) {
-
+    public String myApprovalList(@RequestParam(defaultValue = "1") int page, Model model, HttpSession session) {
+		
+		UserDTO userDTO= (UserDTO)session.getAttribute("loginUser");
+		int loginid = userDTO.getUId();
+		
 		int perPage = 10; // 페이지당 항목 수
 
         List<MyApprovalDTO> posts = myApprovalService.getPaged(page, perPage, loginid);
@@ -39,37 +45,43 @@ public class MyApprovalController {
         model.addAttribute("posts", posts);
         model.addAttribute("currentPage", page);
         model.addAttribute("totalPages", totalPages);
-        return "MyApprovalList";
+        return "myApprovalList";
     }
     
-    @RequestMapping("/testdoc/{dSeq}")
-    public String myApprovalDoc(@PathVariable int dSeq, Model model) {
+	@GetMapping("/testdoc/{dSeq}")
+    public String myApprovalDoc(@PathVariable int dSeq, Model model, HttpSession session) {
+		UserDTO userDTO= (UserDTO)session.getAttribute("loginUser");
+		int loginid = userDTO.getUId();
     	
     	int check = 0;
     	//문서 정보 담기
     	MyApprovalDTO3 DrafterDto = myApprovalService.selectByDSeq(dSeq);
     		
     		
-    	    	check = myApprovalService.nowApproval(dSeq, loginid, DrafterDto.getDDrafterId());
-    	
+    	    check = myApprovalService.nowApproval(dSeq, loginid, DrafterDto.getDDrafterId());
+    	    
     		List<MyApprovalDTO2> ADto = myApprovalService.selectByApprovers(dSeq);
     		model.addAttribute("approver", ADto);
-    		model.addAttribute("mylist", DrafterDto);
     		model.addAttribute("check", check );
     	if(DrafterDto.getDCategory().equals("출장보고서")) {
-    		//버케이션 정보 추가
     		
+    		//model.addAttribute("travel", managementService.getManagementTravel(dSeq));
+    		//model.addAttribute("partnames", managementService.getPartnameWithUsernameAll());
     		return "myApprovalTravel";
     		
     	}else {
-    		
+    		//model.addAttribute("vacation", managementService.getManagementVacation(dSeq));
+    		//model.addAttribute("partnames", managementService.getPartnameWithUsernameAll());
         return "myApprovalVacation";
     	}
     }
     
-    @PostMapping("/dicision")
-    public String getData(@ModelAttribute MyApprovalStateDO DO, Model model) {
-        // Access dataObject.a, dataObject.b, and dataObject.c here
+    @PutMapping("/dicision")
+    public String getData(@ModelAttribute MyApprovalStateDO DO, Model model, HttpSession session) {
+        
+		UserDTO userDTO= (UserDTO)session.getAttribute("loginUser");
+		int loginid = userDTO.getUId();
+    	
         String state = DO.getState();
         int dSeq = DO.getDSeq();
 		MyApprovalDTO3 DrafterDto = myApprovalService.selectByDSeq(dSeq);
