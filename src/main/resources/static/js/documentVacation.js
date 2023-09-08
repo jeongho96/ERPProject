55fffd1f90416a1bc2e13d69f5db2252252d5c8e
ariel-ssen <qhdghl75@gmail.com>
2023-09-08 10:12:04 +0900

    update: 날짜 기능 수정중

--- src/main/resources/static/js/documentVacation.js
+++ src/main/resources/static/js/documentVacation.js
@@ -7,4 +7,7 @@
   const year = today.getFullYear();
   const month = String(today.getMonth() + 1).padStart(2, '0');
   const day = String(today.getDate()).padStart(2, '0');
-  document.getElementById("date").textContent = `${year}년${month}월${day}일`;
+  const formattedDate = `${year}-${month}-${day}`; // 날짜 형식을 yyyy-MM-dd로 맞춤
+  document.getElementById("dDraftingDate").value = formattedDate;
+  /*document.getElementById("dateInput").textContent = `${year}년${month}월${day}일`;*/
+  /*document.getElementById("dateInput").value = ${year}-${month}-${day};*/
