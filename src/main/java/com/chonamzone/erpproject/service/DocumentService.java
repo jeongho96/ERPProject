2d93cfd6e5d4807fa9bc2c44c79837b440224470
ariel-ssen <qhdghl75@gmail.com>
2023-09-08 10:11:29 +0900

    feat: document insert 추가

---
+++ src/main/java/com/chonamzone/erpproject/service/DocumentService.java
@@ -0,0 +1,35 @@
+package com.chonamzone.erpproject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.chonamzone.erpproject.mapper.DocumentListMapper;
+import com.chonamzone.erpproject.mapper.VacationMapper;
+import com.chonamzone.erpproject.model.VacationDTO;
+
+@Service  //서비스라고 적어줘야 서비스라고 알아들음!  //Been 등록 
+public class DocumentService {
+	
+	@Autowired
+	private VacationMapper vacationMapper;
+	/*
+	@Autowired
+	private DocumentListMapper documentListMapper;
+	 */
+	public void insert(VacationDTO.MGVacationDTO post, int loginId) {
+		
+		/*MyApproverDTO3 approverDTO3 = new MyApproverDTO3();*/
+		System.out.println(loginId);
+		vacationMapper.insertDocList(loginId);
+		vacationMapper.insert(post);
+		System.out.println(post.getDSeq());
+		
+//		
+////		반드시 두개 선택해서 approval 입력시킴
+//		vacationMapper.insertApproval(post.getAprvPa1(), post.getAprvName1(), post.getDSeq());
+//		vacationMapper.insertApproval(post.getAprvPa2(), post.getAprvName2(), post.getDSeq());
+//		void insertApproval(int aOrderNum , int aApproverId, int dSeq);
+		
+	}
+
+}
