f69930d79d95e4eea4f4506f162fad8d842acf76
Heesoo <soyamilk0705@naver.com>
2023-09-10 21:31:05 +0900

    feat: 성명 확인 코드 추가

--- src/main/resources/static/js/documentVacation.js
+++ src/main/resources/static/js/documentVacation.js
@@ -1,13 +1,11 @@
-/**
- * 
- */
-
- // 현재 날짜를 가져와서 업데이트
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, '0');
-  const day = String(today.getDate()).padStart(2, '0');
-  const formattedDate = `${year}-${month}-${day}`; // 날짜 형식을 yyyy-MM-dd로 맞춤
-  document.getElementById("dDraftingDate").value = formattedDate;
-  /*document.getElementById("dateInput").textContent = `${year}년${month}월${day}일`;*/
-  /*document.getElementById("dateInput").value = ${year}-${month}-${day};*/
+function signName(){
+	let applicationName = document.getElementById("applicantName").value;
+	let name = document.getElementById("name").value;
+	
+	if(name!=applicationName){
+		alert("성명이 올바르지 않습니다.");
+		return false;
+	}
+	
+	return true;
+}
--- src/main/resources/static/js/managementVacation.js
+++ src/main/resources/static/js/managementVacation.js
@@ -8,3 +8,16 @@ function createPartname(aprvPa, aprvName1, partnameDTO){
 	
 	aprvPa.innerHTML= partnameContents;
 }
+
+
+function signName(){
+	let applicationName = document.getElementById("applicantName").value;
+	let name = document.getElementById("name").value;
+	
+	if(name!=applicationName){
+		alert("성명이 올바르지 않습니다.");
+		return false;
+	}
+	
+	return true;
+}
